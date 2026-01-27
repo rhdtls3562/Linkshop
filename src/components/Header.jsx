e3b1b9adcce0df499c4cc9c19d23791cdc7b5287
권새롬 <rhdtls3562@naver.com>
2026-01-27 16:49:19 +0900

    add : list 페이지 기본 퍼블리싱 초기 완성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,20 @@
-Header
+import React from "react";
+import styles from "./Header.module.css";
+import logo from "../assets/logo.svg";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>
+        <a href="#">
+          <img src={logo} alt="로고" />
+        </a>
+      </h1>
+      <a href="#">
+        <div className={styles.button}>생성하기</div>
+      </a>
+    </header>
+  );
+}
+
+export default Header;
