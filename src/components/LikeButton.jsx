86dded75c39f086f90f63805fe8ce5d9cca2022c
권새롬 <rhdtls3562@naver.com>
2026-01-28 13:50:24 +0900

    feat : 좋아요 기능 추가

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,15 +1,26 @@
 import styles from "./LikeButton.module.css";
 import heartLine from "../assets/heart_line.svg";
 import heart from "../assets/heart.svg";
+import { useState } from "react";
 
 function LikeButton({ count }) {
+  const [isLike, setIsLike] = useState(false);
+
+  const handleClick = () => {
+    return setIsLike(!isLike);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <button className={styles.likes}>
+      <button className={styles.likes} onClick={handleClick}>
         <img src={heartLine} alt="좋아요" />
-        <img src={heart} className={styles.heart} alt="좋아요 활성" />
+        <img
+          src={heart}
+          className={`${styles.heart} ${isLike ? styles.active : ""}`}
+          alt="좋아요 활성"
+        />
       </button>
-      <div className={styles.count}>{count}</div>
+      <div className={styles.count}>{isLike === true ? count + 1 : count}</div>
     </div>
   );
 }
