763caa238172205ca9740c97fd5a673fe3692bde
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-01-28 20:02:46 +0900

    refactor: TextInput 컴포넌트에서 PasswordInput 로직 분리

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,7 +1,4 @@
 import styles from "./Input.module.css";
-import eyeOff from "../assets/btn_visibility_off.svg";
-import eyeOn from "../assets/btn_visibility_on.svg";
-import { useState } from "react";
 
 export function TextInput({
   id,
@@ -12,36 +9,11 @@ export function TextInput({
 }) {
   const classNames = `${styles.input} ${className}`;
 
-  // 비밀번호 보기 버튼
-  const [inputType, setInputType] = useState("password");
-  const handleTogglePassword = () => {
-    setInputType((prevType) => (prevType === "password" ? "text" : "password"));
-  };
-
   return (
     <>
       <div className={styles.wrapper}>
         <label htmlFor={id}>{label}</label>
-        {/* type 속성에 따라 분기 처리 */}
-        {type === "password" ? (
-          <div className={styles.passwordContainer}>
-            <input id={id} type={inputType} className={classNames} {...props} />
-            <button
-              type="button"
-              className={styles.btnEye}
-              onClick={handleTogglePassword}
-            >
-              <img
-                src={inputType === "password" ? eyeOff : eyeOn} // 상태에 따라 아이콘 변경
-                alt={
-                  inputType === "password" ? "비밀번호 보기" : "비밀번호 숨기기"
-                }
-              />
-            </button>
-          </div>
-        ) : (
-          <input id={id} className={classNames} {...props} />
-        )}
+        <input id={id} className={classNames} {...props} />
       </div>
     </>
   );
