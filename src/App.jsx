28b596d7c87df5a105e3ef28477b5a18cae3f1ae
ino0o0o0 <rnldusajqmf@naver.com>
2026-01-29 01:37:30 +0900

    Merge branch 'develop' of https://github.com/rhdtls3562/Linkshop into feature/profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import "./styles/global.css";
 import ShopListPage from "./pages/ShopListPage";
 import Deco from "./components/Deco";
+import Back from "./components/Back";
 function App() {
   return (
     <>
       <Deco /> 
+      <Back onClick={() => window.history.back()} />
       <Routes>
         <Route path="/" element={<ShopListPage />} />
         <Route path="/create" />
