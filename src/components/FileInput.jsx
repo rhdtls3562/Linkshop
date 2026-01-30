77c82ce5c60dfb1a33574b17a940d94b628eb028
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-01-30 15:22:20 +0900

    feat: 파일 첨부 시 미리보기(작업중)

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import styles from "./Input.module.css";
 import ProductImageGrid from "./ProductImageGrid";
-import shopCardStyles from "./ShopCard.module.css";
 
-export function FileInput({ id, label, placeholder, prdList }) {
+export function FileInput({
+  id,
+  label,
+  placeholder,
+  prdList,
+  initialPreview = "",
+}) {
+  const inputRef = useRef();
+  const [file, setFile] = useState();
+  const [preview, setPreview] = useState(initialPreview);
+
+  const handleChange = (e) => {
+    const file = e.target.files?.[0];
+    // const nextFile = e.target;
+    console.log("nextFile:", file);
+    setFile(file);
+  };
+
+  useEffect(() => {
+    if (!file) {
+      setPreview(initialPreview);
+      return;
+    }
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+    console.log("objectUrl: ", objectUrl);
+    return () => {
+      URL.revokeObjectURL(objectUrl);
+    };
+  }, [file]);
+
   return (
     <>
       <div className={styles.wrapper}>
@@ -12,21 +44,15 @@ export function FileInput({ id, label, placeholder, prdList }) {
           파일 첨부
         </label>
         {/* 디자인을 위해 실제 인풋은 숨김 처리 */}
-        <input id={id} type="file" className={styles.fileInput} />
+        <input
+          id={id}
+          type="file"
+          className={styles.fileInput}
+          onClick={handleChange}
+        />
 
         {/* 상품 리스트가 존재할 경우 이미지 미리보기 렌더링 */}
-        {prdList && <ProductImageGrid images={prdList} />}
-        {/* {prdList && (
-          <div className={shopCardStyles.productImages}>
-            {prdList.map((prd) => {
-              return (
-                <div key={prd.id}>
-                  <img src={prd.img} alt="대표 상품" />
-                </div>
-              );
-            })}
-          </div>
-        )} */}
+        {file && <ProductImageGrid images={prdList} />}
       </div>
     </>
   );
