4bee73a5c66bbc63c41380840c1ea5582df23eff
권새롬 <rhdtls3562@naver.com>
2026-01-31 16:00:20 +0900

    fix : header 누락 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-// App.jsx test
+// App.jsx
 import { Routes, Route } from "react-router-dom";
 import "./styles/global.css";
 import ShopListPage from "./pages/ShopListPage";
+import Header from "./components/Header";
 import { LinkPostPage } from "./pages/LinkPostPage";
 import SearchResultPage from "./pages/SearchResultPage";
-import LinkProfilePage from "./pages/LinkProfilePage";
 
 function App() {
   return (
     <>
+      <Header /> {/* 모든 페이지에 공통 Header */}
       <Routes>
         <Route path="/" element={<ShopListPage />} />
         <Route path="/linkpost" element={<LinkPostPage />} />
-        <Route path="/profile" element={<LinkProfilePage />} />
         <Route path="/search" element={<SearchResultPage />} />
       </Routes>
     </>
   );
 }
 
-export default App ;
+export default App;
