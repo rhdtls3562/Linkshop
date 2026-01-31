d42d6b7935935a8865e9976600d434a676690873
권새롬 <rhdtls3562@naver.com>
2026-01-31 23:16:47 +0900

    feat : 이미지 파일 별로 js로 분리

--- src/components/ProductImageGrid.jsx
+++ src/components/ProductImageGrid.jsx
@@ -1,25 +1,9 @@
 import styles from "./ProductImageGrid.module.css";
-
-// 상점 1 이미지
-import shop1_img1 from "../assets/shop1_product1.png";
-import shop1_img2 from "../assets/shop1_product2.png";
-import shop1_img3 from "../assets/shop1_product3.png";
-
-// 상점 2 이미지
-import shop2_img1 from "../assets/shop2_product1.png";
-import shop2_img2 from "../assets/shop2_product2.png";
-import shop2_img3 from "../assets/shop2_product3.png";
+import { SHOP_IMAGE_MAP, SHOP_IMAGES } from "../components/images";
 
 function ProductImageGrid({ shopId }) {
-  // productCount 제거
-  // 상점별 이미지 매핑
-  const shopImageMap = {
-    1052: [shop1_img1, shop1_img2, shop1_img3],
-    1050: [shop2_img1, shop2_img2, shop2_img3],
-  };
-
   // 해당 상점의 이미지 가져오기
-  const images = shopImageMap[shopId] || [shop1_img1, shop1_img2, shop1_img3];
+  const images = SHOP_IMAGE_MAP[shopId] || SHOP_IMAGES.shop1;
 
   return (
     <div className={styles.productImages}>
