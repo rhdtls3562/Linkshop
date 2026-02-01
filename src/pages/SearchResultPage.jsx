fa12951fa46dbbb8c7f56add057eca909dd2967e
여리의갤럭시북\pakjo <kaito5650@naver.com>
2026-02-02 01:03:10 +0900

    feat: 검색 결과에 따른 조건부 렌더링

--- src/pages/SearchResultPage.jsx
+++ src/pages/SearchResultPage.jsx
@@ -2,22 +2,30 @@ import styles from "./SearchResultPage.module.css";
 import SearchBar from "../components/SearchBar";
 import Filter from "../components/Filter";
 import noResult from "../assets/Img_search_null.svg";
+import { useState } from "react";
 
 function SearchResultPage() {
+  const [results, setResults] = useState([]);
+  const hasResult = results.length > 0;
+
   return (
     <div className={styles.container}>
       <SearchBar className={styles.SearchBar} />
       <Filter />
-
-      <div className={styles.no}>
-        <img src={noResult} alt="검색결과 없음" />
-        <div className={styles.font}>
-          <p>검색 결과가 없어요</p>
-          <p>지금 프로필을 만들고 내 상품을 소개해보세요</p>
-        </div>
-      </div>
+      {hasResult ? <p>검색 결과 있음</p> : <NoResult />}
     </div>
   );
 }
 
+function NoResult() {
+  return (
+    <div className={styles.no}>
+      <img src={noResult} alt="검색결과 없음" />
+      <div className={styles.font}>
+        <p>검색 결과가 없어요</p>
+        <p>지금 프로필을 만들고 내 상품을 소개해보세요</p>
+      </div>
+    </div>
+  );
+}
 export default SearchResultPage;
