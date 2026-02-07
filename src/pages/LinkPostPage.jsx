624cb1fed4bb9265bfb070f2abb5946dcb68411e
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-02-07 23:17:22 +0900

    fix: 생성하기 페이지 에러 수정

--- src/pages/LinkPostPage.jsx
+++ src/pages/LinkPostPage.jsx
@@ -98,6 +98,11 @@ export function LinkPostPage() {
 
   return (
     <main className={styles.main}>
+      <div>
+        <p>작업 시간 확인 : 2/7 23:17</p>
+        <p>인풋 자동완성 css 삭제</p>
+        <p>비밀번호 인풋 width 수정</p>
+      </div>
       <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.container}>
           <div className={styles.head}>
